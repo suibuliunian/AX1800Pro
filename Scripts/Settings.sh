639a7150446a1d977a5bdba3b9b6d4599e5401d0
suibuliunian <575066873@qq.com>
2024-09-21 22:54:29 +0800

    add self settings

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -19,29 +19,29 @@ if [[ $WRT_REPO == *"lede"* ]]; then
 	#修改默认时间格式
 	sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $LEDE_FILE
 	#添加编译日期标识
-	sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/\1 \/ $WRT_CI-$WRT_DATE/g" $LEDE_FILE
+	sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/\1 \/ $WRT_DATE/g" $LEDE_FILE
 else
 	#修改immortalwrt.lan关联IP
 	sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 	#添加编译日期标识
-	sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_CI-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
+	sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 fi
 
-#配置文件修改
+#修改默认主题
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 #手动调整的插件
-if [ -n "$WRT_PACKAGE" ]; then
-	echo "$WRT_PACKAGE" >> ./.config
-fi
+# if [ -n "$WRT_PACKAGE" ]; then
+# 	echo "$WRT_PACKAGE" >> ./.config
+# fi
 
 #高通平台锁定512M内存
 if [[ $WRT_TARGET == *"IPQ"* ]]; then
-	echo "CONFIG_IPQ_MEM_PROFILE_1024=n" >> ./.config
-	echo "CONFIG_IPQ_MEM_PROFILE_512=y" >> ./.config
-	echo "CONFIG_ATH11K_MEM_PROFILE_1G=n" >> ./.config
-	echo "CONFIG_ATH11K_MEM_PROFILE_512M=y" >> ./.config
+	echo "CONFIG_IPQ_MEM_PROFILE_1024=y" >> ./.config
+	echo "CONFIG_IPQ_MEM_PROFILE_512=n" >> ./.config
+	echo "CONFIG_ATH11K_MEM_PROFILE_1G=y" >> ./.config
+	echo "CONFIG_ATH11K_MEM_PROFILE_512M=n" >> ./.config
 fi
 
 #科学插件设置
@@ -55,5 +55,5 @@ else
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 
 	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=n" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
 fi
